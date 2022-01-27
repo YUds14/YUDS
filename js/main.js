933a241e337d431eb18d82c08916049a616c6293
YUds14 <yudackovdmit@yandex.ru>
2022-01-27 23:35:14 +0300

    27 evning

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
@@ -6,9 +6,8 @@ const swiper = new Swiper('.swiper', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
-    },
-    effect:'cube'
+      nextEl: '.hotel-slider_button--next',
+      prevEl: '.hotel-slider_button--prev',
+    }
   
   });
